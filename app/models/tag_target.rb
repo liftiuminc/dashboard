3378eb8de6a7cc1d81d7453d91bc6c22f3ac85da
Jos Boumans <jos@liftium.com>
2009-10-22 09:00:20 -0700

    * allow country to be 'blank' to unset it in tag targets   * XXX should this not just destroy the tag targets entry instead? * bugzid: 102

--- app/models/tag_target.rb
+++ app/models/tag_target.rb
@@ -1,7 +1,11 @@
 class TagTarget < ActiveRecord::Base
   belongs_to :tags
 
-  validates_presence_of :key_name, :key_value
+  ### key_NAME may not be blank, key_value may be blank, in some cases
+  validates_presence_of :key_name 
+  validates_presence_of :key_value, 
+                        :unless => Proc.new { |v| v['key_name'] == 'country' }
+  
   validates_uniqueness_of :key_name, {:scope => :tag_id}
 
 end
