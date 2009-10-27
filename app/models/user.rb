a50153161957a87d74249dff2eb8c14c59659e8b
Nick Sullivan <nick@liftium.com>
2009-10-26 17:58:22 -0700

    must_associate_publisher_if_not_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
   acts_as_authentic
   belongs_to :publisher
 
+  validate :must_associate_publisher_if_not_admin
+
+  def must_associate_publisher_if_not_admin
+    if !admin and publisher_id.blank? 
+      errors.add_to_base("If the user is not an administrator, a publisher must be selected")
+    end
+  end
+
   def deliver_password_reset_instructions!
     reset_perishable_token!
     Notifier.deliver_password_reset_instructions(self)
