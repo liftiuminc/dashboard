9c9bb41cd003849943f6d03adfd3a9576627c374
Jos Boumans <jos@liftium.com>
2009-10-27 16:48:16 -0700

    * add before_save hooks, distinguish on pay types

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -2,8 +2,6 @@ class Revenue < ActiveRecord::Base
     belongs_to :tag
     belongs_to :user
 
-    attr_accessible :attempts, :rejects, :clicks, :revenue, :ecpm, :day
-
     validates_uniqueness_of :day, :scope => :tag_id
 
     ### ints
@@ -23,4 +21,51 @@ class Revenue < ActiveRecord::Base
     end
 
     validates_date :day
+    validates_numericality_of   :tag_id,    :greater_than => 0
+    validates_numericality_of   :user_id,   :greater_than => 0
+    
+    validates_each :tag_id do |record, attr, value| 
+        record.errors.add attr, "No such tag #{value}" unless Tag.find(value)
+    end
+
+    validates_each :user_id do |record, attr, value| 
+        record.errors.add attr, "No such user #{value}" unless User.find(value)
+    end       
+
+    ### fix up the record so we have all our fields in order
+    ### also verify settings based on pay type
+    before_save { |r|
+        ### depending on pay type, we need to use a different method
+        ### XXX FIXME some duplication here, clean me up
+
+        if r.tag.network.pay_type == 'Per Click' 
+
+            ### calculate missing ecpm
+            if r.revenue and r.clicks and ( not r.ecpm or not r.ecpm.length )
+                r.ecpm = ( r.revenue / r.clicks ) * 1000
+
+            ### calculate missing attempts
+            elsif r.revenue and r.ecpm and ( not r.clicks or not r.clicks.length )
+                r.clicks = ( r.revenue * r.ecpm ) / 1000
+            end
+
+            ### per click networks will never reject
+            r.rejects = 0
+            
+        elsif r.tag.network.pay_type == 'Per Impression'             
+
+            ### calculate missing ecpm
+            if r.revenue and r.attempts and ( not r.ecpm or not r.ecpm.length )
+                r.ecpm = ( r.revenue / r.attempts ) * 1000
+
+            ### calculate missing attempts
+            elsif r.revenue and r.ecpm and ( not r.attempts or not r.attempts.length )
+                r.attempts = ( r.revenue * r.ecpm ) / 1000
+            end
+        
+        end
+
+        ### format the date
+        r.day = r.day.to_date.to_s
+    }
 end
