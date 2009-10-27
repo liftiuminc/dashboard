600f5af73517a59b0a169435eee78f98920e07e1
Jos Boumans <jos@liftium.com>
2009-10-27 14:30:26 -0700

    * add validators & tests for revenues * add the links from tags & users

--- test/unit/revenue_test.rb
+++ test/unit/revenue_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
+require 'pp'
 
 class RevenueTest < ActiveSupport::TestCase
-  should "be valid" do
-    assert Revenue.new.valid?
-  end
+    should_belong_to :tag
+    should_belong_to :user
+
+    should "be valid" do
+        assert Revenue.new( :day => "2004-01-01", :tag => "1" ).valid?
+    end
+    
+    ### %w makes an array, *( ) derefences it again
+    %w( attempts rejects clicks ).each do |f|
+        should_allow_values_for f,      *( %w( 0 1 9999999 ) )
+        should_not_allow_values_for f,  *( %w( -1 1.5 1,24 foo ) )       
+    end        
+    
+    %w( ecpm revenue ).each do |f|
+        should_allow_values_for f,      *( %w( 0 1 1.5 9999999 ) )
+        should_not_allow_values_for f,  *( %w( -1 1,24 foo ) )       
+    end
+    
+    %w( day ).each do |f|
+        should_allow_values_for f,      "2004-01-01", "10/01/2009", "1-1-1980"
+        should_not_allow_values_for f,  "foo", "42"
+    end        
+    
 end
