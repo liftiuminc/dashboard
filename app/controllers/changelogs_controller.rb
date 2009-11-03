24ef24f4f975b373815f8b78a68a7d6ad4952b1f
Scott Tamosunas <tamosunas@gmail.com>
2009-11-03 13:06:48 -0800

    Added a dropdown box to filter the number of entries shown on the Changelogs page.

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -8,12 +8,15 @@ class ChangelogsController < ApplicationController
   private
 
   def find_changelogs(params)
+    @entries = "all"
+    limit = {:limit => @entries = params[:entries].to_i} if params[:entries] && params[:entries] != "all"
+
     if params[:record_id] && params[:record_type]
-      Changelog.find_all_by_record_id_and_record_type(params[:record_id], params[:record_type])
+      Changelog.find_all_by_record_id_and_record_type(params[:record_id], params[:record_type], limit)
     elsif params[:user_id]
-      Changelog.find_all_by_user_id(params[:user_id])
+      Changelog.find_all_by_user_id(params[:user_id], limit)
     else
-      Changelog.find(:all)
+      Changelog.find(:all, limit)
     end
   end
 end
