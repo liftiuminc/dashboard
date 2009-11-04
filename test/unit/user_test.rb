7076edc6fe810a7b924001a96a8aba0b8c82e17b
Nick Sullivan <nick@liftium.com>
2009-11-03 16:04:41 -0800

    remove test for changeloggable from users

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  should_acts_as_changelogable do
-    User.create!(:email => "joe@myemail.com", :publisher_id => 1, :admin => false, :password => "password",
-                 :password_confirmation => "password")
-  end
 end
