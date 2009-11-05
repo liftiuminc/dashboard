45058bd5d7675378315c858a8844569b3ece4b67
Jos Boumans <jos@liftium.com>
2009-11-05 03:00:55 +0000

    * add basic tests for the movers controller

--- test/functional/movers_controller_test.rb
+++ test/functional/movers_controller_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class MoversControllerTest < ActionController::TestCase
-  context "index action" do
-    should "render index template" do
-      get :index
-      assert_template 'index'
+  setup :activate_authlogic
+ 
+  DateRangeHelper.timeframes.each do |timeframe|
+    context "index action" do
+      should "render for '#{timeframe}'" do
+        login_as_admin
+        get :index, params = { :date_select => timeframe }
+        assert_response :success
+        assert_template :index
+      end
     end
   end
 end
