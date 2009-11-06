75d7615e79ade140626827e8023dad6b245b5b72
Nick Sullivan <nick@liftium.com>
2009-11-06 13:06:53 -0800

    Publisher tags wasn't the right idea

--- app/controllers/publisher_tags_controller.rb
+++
@@ -1,8 +0,0 @@
-class PublisherTagsController < ApplicationController
-  before_filter :require_user
-  
-  def index
-    @publisher_tags = Tag.all(:conditions => ["publisher_id = ?", current_user.publisher_id])
-  end
-  
-end
