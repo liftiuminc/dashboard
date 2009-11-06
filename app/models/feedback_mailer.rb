66ef556c0a356fc759d6de47e04034a4c96b41bc
Nick Sullivan <nick@liftium.com>
2009-11-06 00:57:54 -0800

    fix replyto

--- app/models/feedback_mailer.rb
+++ app/models/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ActionMailer::Base
   
   def feedback(feedback)
     @recipients  = 'nick@liftium.com'
-    @from        = 'noreply@yoursite.com'
+    @from        = 'noreply@liftium.com'
     @subject     = "[Feedback for Liftium] #{feedback.subject}"
     @sent_on     = Time.now
     @body[:feedback] = feedback    
