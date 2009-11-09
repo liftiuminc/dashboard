ba10fb0b532cf14d83ec194c57c6bd1d43511098
Scott Tamosunas <tamosunas@gmail.com>
2009-11-09 17:02:37 -0500

    remove the acts_as_changelogable. Prepare for a git submodule

--- vendor/plugins/acts_as_changelogable/init.rb
+++
@@ -1,6 +0,0 @@
-$:.unshift "#{File.dirname(__FILE__)}/lib"
-require 'model/changelog'
-require 'active_record/acts/changelogable'
-
-ActiveRecord::Base.class_eval { include ActiveRecord::Acts::Changelogable }
-
