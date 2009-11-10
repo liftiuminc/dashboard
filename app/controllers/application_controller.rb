338773ecfa2324df44e1d5598cdb96dca0652dcb
Jos Boumans <jos@liftium.com>
2009-11-10 22:40:53 +0000

    * add timezone support  * Timezone can now be set on a per user basis (default pacific)  * Activerecord does the needed conversions  * /homes/publishers appear to show the right dates/times on the charts  * Followed this howto:    http://mad.ly/2008/04/09/rails-21-time-zone-support-an-overview/ * bugzid: 129

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user
 
   before_filter :set_acts_as_changelogable_current_user
+  before_filter :set_time_zone
 
   private
 
@@ -32,6 +33,10 @@ class ApplicationController < ActionController::Base
       @current_publisher = @current_user.publisher
     end
   end
+
+  def set_time_zone
+    Time.zone = @current_user.time_zone if current_user
+  end  
 
   def require_user
     unless current_user
