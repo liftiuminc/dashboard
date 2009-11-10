1d8fdf8bc283de70415a7b4c7047e0011d471650
Nick Sullivan <nick@liftium.com>
2009-11-10 01:33:07 -0800

    Fix up the stats a bit for publisher home. Hide the revenue by default for publishers until we have data filled in regularly

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -32,13 +32,15 @@ class HomesController < ApplicationController
 
     @dates = DateRangeHelper.get_date_range(params[:date_select])
     
-    
-    if @dates[0].to_time < (DateTime.now - 7.days)
+    if @dates[0].to_time > (DateTime.now - 1.hour)
+        model = FillsMinute
+	time_format = "%I:%M:p"
+    elsif @dates[0].to_time > (DateTime.now - 7.days)
+        model = FillsHour
+	time_format = "%I:%p"
+    else
         model = FillsDay
 	time_format = "%m/%d"
-    else
-        model = FillsHour
-	time_format = "%H:%p"
     end
 
     
@@ -47,7 +49,7 @@ class HomesController < ApplicationController
     col = model.new.time_column
     sql = "SELECT * FROM #{model.table_name}"+
         " WHERE tag_id IN (SELECT id FROM tags where publisher_id = ?)" +
-        " AND #{col} >= ?  AND #{col} <= ? ORDER by #{col} DESC"
+        " AND #{col} >= ?  AND #{col} <= ? GROUP BY #{col} ORDER by #{col} DESC"
         
     @stats = model.find_by_sql [sql, @publisher.id, @dates[0], @dates[1]]
     @previous_stats = model.find_by_sql [sql, @publisher.id, @dates[2], @dates[0]]
