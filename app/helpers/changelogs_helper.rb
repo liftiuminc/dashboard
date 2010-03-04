922c069f632d1be20608ad066b3f4e34f92b9e37
Nick Sullivan <nick@liftium.com>
2010-03-04 18:28:30 -0500

    clean up messages

--- app/helpers/changelogs_helper.rb
+++ app/helpers/changelogs_helper.rb
@@ -23,9 +23,9 @@ module ChangelogsHelper
 
   def changelogs_title(params)
     if params[:record_id] && params[:record_type]
-      "Changelogs - Filtered by #{params[:record_type]} with id #{params[:record_id]} (#{link_to("Show All", changelogs_path)})"
+      "Changelogs - Filtered by #{params[:record_type]} with id ##{params[:record_id]}"
     elsif params[:user_id] && user = User.find_by_id(params[:user_id])
-      "Changelogs - Filtered by #{user.email}'s changes (#{link_to("Show All", changelogs_path)})"
+      "Changelogs - Filtered by #{user.email}'s changes)"
     else
       title = "Changelogs - Showing #{@entries || "all"} entries"
       title += " excluding #{User.find_by_id(params[:excluded_user_id]).email} (#{link_to("Show All", changelogs_path)})" if params[:excluded_user_id] && params[:excluded_user_id] != "none"
