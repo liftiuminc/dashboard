19db9edb10ec6e50c1c8472a09e645dca4438d7a
Nick Sullivan <nick@liftium.com>
2010-04-23 17:39:24 -0400

    initial api

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,35 @@
+class ApiController < ApplicationController
+
+  def update_tag 
+    if !params[:id] 
+      die_with_error("Must supply a tag id");
+
+    else  
+      @tag = Tag.find(params[:id])
+      
+      tagParams = {}
+      for key in @tag.attribute_names do
+        tagparams[key] = params[key] if params[key]
+      end
+
+      if @tag.update_attributes(tagParams)
+        respond_to do |format|
+          format.html { render :text => @tag.inspect, :content_type => "text/plain" }
+          format.json { render :json => @tag.to_json }
+          format.xml { render :xml => @tag.to_xml }
+        end  
+      else 
+        die_with_error(@tag.errors)
+      end
+    end
+  end
+
+  def die_with_error (msg)
+    respond_to do |format|
+      format.html { render :text => msg.inspect, :status => 400, :content_type => "text/plain" }
+      format.json { render :json => msg.to_json, :status => 400 }
+      format.xml { render :xml => msg.to_xml, :status => 400 }
+    end  
+  end
+
+end
