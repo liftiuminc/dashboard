8bd3fe435952be5a7ea7c7d6c40cf16485434bbe
Nick Sullivan <nick@kruxdigital.com>
2010-08-07 13:26:45 -0700

    make brand_safety_level a required field

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -15,6 +15,7 @@ class Publisher < ActiveRecord::Base
 
   validates_uniqueness_of :site_name
   validates_presence_of :site_name
+  validates_presence_of :brand_safety_level
   validates_presence_of :site_url
   validates_format_of :xdm_iframe_path, :with => /^\/[^ ]+/, :allow_blank => true, :message => "must be a local path Ex. /liftium_iframe.html"
   validates_numericality_of :beacon_throttle, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 1
